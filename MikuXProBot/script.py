469d26ed283462ad0525e94417e93c0f9770a187
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-05-07 12:32:59 +0530

    Initialize Shukurenai-1

---
+++ MikuXProBot/script.py
@@ -0,0 +1,44 @@
+PM_START_TEXT = """────「 [{}]({}) 」────\n*Hello {}!*\n*I'm An Anime Themed Advance Group Management Bot*\n\n❍ *Uptime:* `{}`\n❍ `{}` *Users, Across* `{}` *Chats*\n\n*Click The Help Button To Get List Of My Commands.*\Thanks To @Shukurenaiprobot For Repo"""
+
+MIKU_IMG = (
+      "https://telegra.ph/file/4f17fdcc9470cc62b8c09.jpg",
+      "https://telegra.ph/file/e34e99801eb78ed84cb93.jpg",
+      "https://telegra.ph/file/917359f33c042a0425a00.jpg",
+      "https://telegra.ph/file/ca91b17ebf942ca00d9a2.jpg",
+      "https://telegra.ph/file/686b843ef19f3deafec37.jpg",
+      "https://telegra.ph/file/04d0f3e168ecd407c11ee.jpg",
+      "https://telegra.ph/file/6d3f5d3c434282a42a9a9.jpg",
+      "https://telegra.ph/file/d1572c4fcace1ae1bc85a.jpg",
+      "https://telegra.ph/file/f74dde459c5079c80d4bf.jpg",
+      "https://telegra.ph/file/010f3e703df0501305498.jpg",
+      "https://telegra.ph/file/7a10635105006681efb13.jpg",
+      "https://telegra.ph/file/6f8529c890740533f942a.jpg",
+      "https://telegra.ph/file/981c0edfa6fc49248c734.jpg",
+      "https://telegra.ph/file/1c24b51d7d1830ee5b626.jpg",
+      "https://telegra.ph/file/6513527f063c4f00a6806.jpg",
+)
+
+TEXXT = "<b>Heya</b> {} <b>Do you Like me?</b>"
+
+MIKU_N_IMG = (
+      "https://telegra.ph/file/4f17fdcc9470cc62b8c09.jpg",
+      "https://telegra.ph/file/e34e99801eb78ed84cb93.jpg",
+      "https://telegra.ph/file/917359f33c042a0425a00.jpg",
+      "https://telegra.ph/file/ca91b17ebf942ca00d9a2.jpg",
+      "https://telegra.ph/file/686b843ef19f3deafec37.jpg",
+      "https://telegra.ph/file/04d0f3e168ecd407c11ee.jpg",
+      "https://telegra.ph/file/6d3f5d3c434282a42a9a9.jpg",
+      "https://telegra.ph/file/d1572c4fcace1ae1bc85a.jpg",
+      "https://telegra.ph/file/f74dde459c5079c80d4bf.jpg",
+      "https://telegra.ph/file/010f3e703df0501305498.jpg",
+      "https://telegra.ph/file/7a10635105006681efb13.jpg",
+      "https://telegra.ph/file/6f8529c890740533f942a.jpg",
+      "https://telegra.ph/file/981c0edfa6fc49248c734.jpg",
+      "https://telegra.ph/file/1c24b51d7d1830ee5b626.jpg",
+      "https://telegra.ph/file/6513527f063c4f00a6806.jpg"
+
+)
+
+PM_PHOTO = "https://telegra.ph/file/6513527f063c4f00a6806.jpg"
+
+MIKU_DISPACHER_PIC = "https://telegra.ph/file/981c0edfa6fc49248c734.jpg"
